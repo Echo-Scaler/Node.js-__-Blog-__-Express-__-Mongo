ec8748de6d45e229a4bacb609b7ceb8f5968aa72
*0iGWgdvTWg <waiyan.toshima12@gmail.com>
2026-02-05 18:02:42 +0900

    render-ejs-static-partials-UI

--- app.js
+++ app.js
@@ -5,22 +5,31 @@ const expressLayouts = require("express-ejs-layouts");
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
-app.use(express.static('public'));
+app.use(express.static('public')); /* middleware အဖြစ်သုံးထားတာပါ။ */
 
 
 //Templating Engine
 app.use(expressLayouts);
-app.set("layout", "./layouts/main");
+app.set("layout", "./layouts/main"); //default layout file name is maine.js
 app.set("view engine", "ejs");
 
+// EJS မှာ layout system မပါဘူး
+// Header / Footer ကို page တစ်ခုချင်း include မလုပ်ချင်တဲ့အခါ
+// Common layout တစ်ခုတည်းကို pages အားလုံးမှာ အသုံးချချင်လို့ သုံးပါတယ် -->
 
 
-
-
-
+// separate routes in server/routes/main.js
 app.use("/", require("./server/routes/main"));
+
+
+
+
+
+
+
+
 
 
 
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();   //need to imoport for routes
+const router = express.Router(); //need to imoport for routes
 
 
 
@@ -7,7 +7,18 @@ const router = express.Router();   //need to imoport for routes
 
 
 router.get("/", (req, res) => {
-    res.send("Hello World!");
+  const locals = {
+    title: "BlogNinja",
+    description: "BlogNinja is a blog website Testing Page",
+  };
+  res.render("index", { locals });
+});
+
+router.get("/about", (req, res) => {
+  res.render("about");
+});
+router.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 module.exports = router;
